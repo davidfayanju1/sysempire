cb2149025eec29d11039b5d006c9fc9f7da4592c
davidfayanju1 <davidfayanju01@gmail.com>
2026-04-26 00:59:43 +0100

    UI updates

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,19 @@ import Home from "./pages/home";
 import ContactUs from "./pages/contact-us";
 import AboutPage from "./pages/about-page";
 import Collections from "./pages/collections";
+import ScrollToTop from "./lib/ScrollToTop";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/contact" element={<ContactUs />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="/collection" element={<Collections />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/collection" element={<Collections />} />
+      </Routes>
+    </>
   );
 }
 
